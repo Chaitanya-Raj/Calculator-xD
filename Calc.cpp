f25419dc04d3f46056fa3a958ac9bb1e6a4d93d8
Chaitanya-Raj <41683865+Chaitanya-Raj@users.noreply.github.com>
2018-08-23 22:19:17 +0530

    Formatting

--- Calc.cpp
+++ Calc.cpp
@@ -1,75 +1,105 @@
 #include<iostream>
 #include<conio.h>
+#include<process.h>
 
 using namespace std;
 
 double x,y;
 
+void basic_input()
+{
+	cout<<"\nEnter the first number : ";
+	cin>>x;
+	cout<<"\nEnter the second number : ";
+	cin>>y;
+}
+
 void addition()
 {
+	basic_input();
 	float sum;
 	sum=x+y;
-	cout<<"Sum = "<<sum;
+	cout<<"\nSum = "<<sum;
 }
 
 void subtraction()
 {
+	basic_input();
 	float dif;
 	dif=x-y;
-	cout<<"Difference = "<<dif;
+	cout<<"\nDifference = "<<dif;
 }
 
 void multiplication()
 {
+	basic_input();
 	float prod;
 	prod=x*y;
-	cout<<"Product = "<<prod;
+	cout<<"\nProduct = "<<prod;
 }
 
 void division()
 {
-	float quo;
-	quo=x/y;
-	cout<<"Quotient = "<<quo;
+	basic_input();
+	if(y==0)
+	{
+		cout<<"\nDivision by zero is not defined";
+	}
+	else
+	{
+		float quo;
+		quo=x/y;
+		cout<<"\nResult = "<<quo;
+	}
 }
 
-void modulus()
+void modulusx()
 {
+	basic_input();
 	int a,b,rem;
 	a=int(x);
 	b=int(y);
 	rem=a%b;
-	cout<<"Remainder = "<<rem;
+	cout<<"\nRemainder = "<<rem;
 }
 
 int main()
 {
-	char ch;
-	cout<<"\nEnter the first number :";
-	cin>>x;
-	cout<<"\nEnter the second number :";
-	cin>>y;
-	cout<<"\nChoose an Operation";
-	cout<<"\n1.Addition(+)";
-	cout<<"\n2.Subtraction(-)";
-	cout<<"\n3.Multiplication(*)";
-	cout<<"\n4.Division(/)";
-	cout<<"\n5.Modulus(%)\n\n";
-	cin>>ch;
-	switch(ch)
+	char flag;
+	int ch;
+	do
 	{
-		case 1:addition();
-					break;
-		case 2:subtraction();
-					break;
-		case 3:multiplication();
-					break;
-		case 4:division();
-					break;
-		case 5:modulus();
-					break;
-		default:cout<<"Enter a Valid Choice !!!";
-	}
-	getch();
+		cout<<"\n\n\n";
+		for(int i=0;i<80;i++)
+		{
+			cout<<"_";
+		}
+		cout<<"\n\n\nChoose an Operation\n";
+		cout<<"\n1.Addition(+)";
+		cout<<"\n2.Subtraction(-)";
+		cout<<"\n3.Multiplication(*)";
+		cout<<"\n4.Division(/)";
+		cout<<"\n5.Modulus(%)";
+		cout<<"\n0.Exit\n\n\n";
+		cin>>ch;
+		switch(ch)
+		{
+			case 1:addition();
+						break;
+			case 2:subtraction();
+						break;
+			case 3:multiplication();
+						break;
+			case 4:division();
+						break;
+			case 5:modulusx();
+						break;
+			case 0:exit(0);
+			default:cout<<"\nEnter a Valid Choice !!!";
+		}
+		cout<<"\n\nDo you want to perform another operation? ";
+		cin>>flag;
+	}while(flag=='y'||flag=='Y');
 	return 0;
 }
+	
